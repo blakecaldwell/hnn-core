58265939b362e11f7a21a199a56e5c442a225f5b
Blake Caldwell <blake_caldwell@brown.edu>
2019-05-20 20:12:33 -0400

    Enable NEURON sims to use available processors

--- mne_neuron/parallel.py
+++ mne_neuron/parallel.py
@@ -10,7 +10,7 @@ def create_parallel_context(n_jobs=1):
     global rank, nhosts, cvode, pc
     nhosts = n_jobs
     rank = 0
-    pc = h.ParallelContext(nhosts)  # MPI: Initialize the ParallelContext class
+    pc = h.ParallelContext()  # MPI: Initialize the ParallelContext class
     pc.done()
     nhosts = int(pc.nhost())  # Find number of hosts
     rank = int(pc.id())     # rank or node number (0 will be the master)
