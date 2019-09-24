5efcb548c477d96a6555f969918534a777756836
Blake Caldwell <blake_caldwell@brown.edu>
2019-09-24 14:49:40 -0400

    TST: add test for MPI and Network refactoring
    Testing with MPI is a little tricky because separate processes need
    to be spawned. Also hyperthreading (Intel procs) will give a false
    number of cpus with multiprocessing.cpu_count. We need to use psutil
    to get the number of physical cores that MPI can actually use.
    
    Also build fewer NEURON components in .travis.yml

--- hnn_core/tests/test_network.py
+++ hnn_core/tests/test_network.py
@@ -13,7 +13,7 @@ def test_network():
     params_fname = op.join(hnn_core_root, 'param', 'default.json')
     params = read_params(params_fname)
     net = Network(deepcopy(params))
-    net.build_in_neuron()
+    net.build_in_neuron()  # needed to populate net.cells
     for p in params:
         assert params[p] == net.params[p]
     assert len(params) == len(net.params)
