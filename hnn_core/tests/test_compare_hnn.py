3e5b8975aae2710918d6fcd6ddd732f86ace366d
Blake Caldwell <blake_caldwell@brown.edu>
2020-02-26 17:35:17 -0500

    MAINT: make test_hnn_core MPI-aware and reuse

--- hnn_core/tests/test_compare_hnn.py
+++ hnn_core/tests/test_compare_hnn.py
@@ -5,11 +5,11 @@ from numpy.testing import assert_array_equal
 
 from mne.utils import _fetch_file
 import hnn_core
-from hnn_core import simulate_dipole, Network, read_params
+from hnn_core import simulate_dipole, Network, read_params, get_rank
 
 
-def test_hnn_core():
-    """Test to check if MNE neuron does not break."""
+def test_hnn_core(n_jobs=1):
+    """Test to check if hnn-core does not break."""
     # small snippet of data on data branch for now. To be deleted
     # later. Data branch should have only commit so it does not
     # pollute the history.
@@ -27,12 +27,19 @@ def test_hnn_core():
 
     # run the simulation
     net = Network(params)
-    dpl = simulate_dipole(net)[0]
+    dpl = simulate_dipole(net, n_jobs=n_jobs)[0]
 
-    # write the dipole to a file and compare
-    fname = './dpl2.txt'
-    dpl.write(fname)
+    if get_rank() == 0:
+        # write the dipole to a file and compare
+        fname = './dpl2.txt'
+        dpl.write(fname)
 
-    dpl_pr = loadtxt(fname)
-    assert_array_equal(dpl_pr[:, 2], dpl_master[:, 2])  # L2
-    assert_array_equal(dpl_pr[:, 3], dpl_master[:, 3])  # L5
+        data_url = ('https://raw.githubusercontent.com/jonescompneurolab/'
+                    'hnn-core/test_data/dpl.txt')
+        if not op.exists('dpl.txt'):
+            _fetch_file(data_url, 'dpl.txt')
+        dpl_master = loadtxt('dpl.txt')
+
+        dpl_pr = loadtxt(fname)
+        assert_array_equal(dpl_pr[:, 2], dpl_master[:, 2])  # L2
+        assert_array_equal(dpl_pr[:, 3], dpl_master[:, 3])  # L5
